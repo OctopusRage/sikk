eb2d3b6199b932e2fab5608c3efa7872ee86180c
AjiTirta <octopusRage@localhost.localdomain>
2016-10-08 01:23:28 -0400

    add logic for admin login controller

--- app/controllers/api/v1/admin/sessions_controller.rb
+++ app/controllers/api/v1/admin/sessions_controller.rb
@@ -1,3 +1,33 @@
-class SessionsController < ApplicationController
-	
+module API
+  module V1
+    module Admins
+			class SessionsController < ApplicationController
+				def create
+					password = params[:password]
+			    email = params[:email]
+			    user = Admin.find_by(authentication_token:params[:email])
+			    if user.present? 
+			    	render json: {
+			    		status: 'fail',
+			    		message: 'invalid username or password'
+			    	}
+			    else
+				    if user.valid_password? password
+				      sign_in user, store: false
+				      user.save
+				      render json: {
+				      		status: 'success',
+				      		data: user
+				      	}, status: 200
+				    else
+				      render json: { 
+				      	status: 'fail',
+				      	message: 'invalid username or password'
+				      }, status: 422
+				    end
+				  end
+				end	
+			end
+		end
+	end
 end
