dafd910ce990aeac66c4b930b1cbc6728924ff65
AjiTirta <octopusRage@localhost.localdomain>
2016-10-08 12:46:30 -0400

    asd

--- app/controllers/api/v1/admins/reports_controller.rb
+++ app/controllers/api/v1/admins/reports_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Admins::ReportsController < AdminController
 		laporan = Laporan.all
 		total_data = laporan.count
 		laporan = laporan.page(params[:page]) if params[:page]
-		laporan = laporan.where(id: params[:id_category]) if params[:id_category]
+		laporan = laporan.where(report_category_id: params[:id_category]) if params[:id_category]
 		laporan = laporan.where(village_id: params[:village_id]) if params[:village_id]
 		render json: {
 			status: 'success',
