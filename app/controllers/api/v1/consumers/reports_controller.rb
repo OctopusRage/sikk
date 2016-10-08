f3f30fdf732edfa62a7fc05e9594f7d635a1c652
AjiTirta <octopusRage@localhost.localdomain>
2016-10-08 11:25:15 -0400

    fixing mistype

--- app/controllers/api/v1/consumers/reports_controller.rb
+++ app/controllers/api/v1/consumers/reports_controller.rb
@@ -7,30 +7,24 @@ class Api::V1::Consumers::ReportsController < ApplicationController
 			data: laporan
 		}, status: 200
 	end
+
 	def create
 		laporan = Laporan.new(report_params)
-		if laporan.valid?
-			if laporan.save
-				render json: {
-					status: 'success',
-					data: laporan
-				}, status: 201
-			else
-				render json: {
-					status: 'fail',
-					message: "#{laporan.errors}"
-				}, status: 422
-			end
+		if laporan.save
+			render json: {
+				status: 'success',
+				data: laporan
+			}, status: 201
 		else
 			render json: {
 				status: 'fail',
-				message: 'failed to post'
+				message: "#{laporan.errors.full_messages.first}"
 			}, status: 422
 		end
 	end	
 	private
 		def report_params
-			params.require(:laporan).permit(:consumer_id, :title, :vilage_id, :latitude, :longitude, :process, :verified_by, :content,
+			params.require(:laporan).permit(:consumer_id, :title, :village_id, :latitude, :longitude, :process, :verified_by, :content,
 			:additional_info, :report_category_id)
 		end
 		
