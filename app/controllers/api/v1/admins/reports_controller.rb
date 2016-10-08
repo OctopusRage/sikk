d3d858da80c2a353e7ab7395acf5b63a22013c63
AjiTirta <octopusRage@localhost.localdomain>
2016-10-08 11:51:41 -0400

    add pagination in report

--- app/controllers/api/v1/admins/reports_controller.rb
+++ app/controllers/api/v1/admins/reports_controller.rb
@@ -2,10 +2,15 @@ class Api::V1::Admins::ReportsController < AdminController
 	before_action :authorize_admin
 	def index
 		laporan = Laporan.all
+		total_data = laporan.count
 		laporan = laporan.page(params[:page]) if params[:page]
 		render json: {
 			status: 'success',
-			data: laporan
+			data: {
+				total_data: total_data,
+				count: laporan.count,
+				report: laporan
+			}
 		}, status: 200
 	end
 end
