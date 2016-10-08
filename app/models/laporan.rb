7b3749d7688ee4c035153a8924e1c9ffb39fd1b5
AjiTirta <octopusRage@localhost.localdomain>
2016-10-08 12:13:28 -0400

    add pagination in report

--- app/models/laporan.rb
+++ app/models/laporan.rb
@@ -9,4 +9,40 @@ class Laporan < ActiveRecord::Base
 
 	has_many :file_uploads, as: :uploader
 	has_many :report_categories
+
+  def load_village
+    village = VillageClient.get_data(self.area_id)
+    if !village.nil?
+      if village["count"]["total"] == 1
+        village["desa"][0]
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
+
+	def as_json(options={})
+    village = load_village
+    village_name = village["kelurahan_desa"] if !village.nil?
+    city = village["kabupaten_kota"] if !village.nil?
+    kecamatan = village["kecamatan"] if !village.nil?
+    province = village["propinsi"] if !village.nil?
+    {
+      consumer_id: consumer_id,
+      title: title,
+      content: content,
+      additional_info: additional_info,
+      latitude: latitude, 
+      longitude: longitude,
+      village_id: village_id,
+      village_name: village_name,
+      city: city,
+      kecamatan: kecamatan,
+      province: province,
+      process: process,
+      verified_by: verified_by
+    }
+  end
 end
